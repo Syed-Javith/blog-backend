1c1e0c72020623118b56d8a7b5e5023c7c914d02
Syed-Javith <syedjavith14@gmail.com>
2023-10-30 00:52:32 +0530

    finished

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -1,7 +1,7 @@
 const cors = require('cors');   
 
 const corsMiddleware = cors({
-    origin : ["http://localhost:3000","http://localhost:4200", ],
+    origin : ["http://localhost:3000","http://localhost:4200", 'http://localhost:19006', 'https://blog-frontend-deployed.netlify.app/'],
     methods : ['GET' , 'POST' , 'PATCH' , 'DELETE'],
     credentials : true,
     allowedHeaders : ['Content-Type','Authorization']
